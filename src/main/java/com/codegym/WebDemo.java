9397f67c150d0b202778afeaf8f60e9228cea684
scorpio611 <evilmonter666@gmail.com>
2018-12-24 09:29:25 +0700

    [Thực hành] Tạo trang web hiển thị thời gian hệ thống

---
+++ src/main/java/com/codegym/WebDemo.java
@@ -0,0 +1,20 @@
+package com.codegym;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+
+public class WebDemo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Date date = new Date();
+        response.getWriter().print(date);
+    }
+}
